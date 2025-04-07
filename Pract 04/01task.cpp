fd427268cfed7097eac82569b060441f58692411
Viki <91424361+viki441@users.noreply.github.com>
2025-04-07 11:49:16 +0300

    Update 01task.cpp

--- Pract 04/01task.cpp
+++ Pract 04/01task.cpp
@@ -57,9 +57,6 @@ void extractNumbers(int* newArr, std::ifstream& infile, int* evens, int* odds, i
         }
     }
 
-#include <iostream>
-#include <fstream> 
-
 int main() {
     const int SIZE = 10;
     int arr[SIZE] = { 1,2,3,4,5,6,7,8,9,10 };
