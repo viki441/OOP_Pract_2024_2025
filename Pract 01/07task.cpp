dcbe94202e0bcaad0f894badf21c8a6ac9d1fdac
Viki <91424361+viki441@users.noreply.github.com>
2025-05-05 10:01:12 +0300

    Update 07task.cpp

--- Pract 01/07task.cpp
+++ Pract 01/07task.cpp
@@ -2,14 +2,14 @@
 struct Complex {
 	int real;
 	int imaginary;
-	
+
 };
-Complex* sumOfComplexNumbers(Complex number1, Complex number2) {
-	Complex* sum = new Complex{ number1.real + number2.real, number1.imaginary + number2.imaginary};
+Complex sumOfComplexNumbers(const Complex& number1, const Complex& number2) {
+	Complex sum { number1.real + number2.real, number1.imaginary + number2.imaginary };
 	return sum;
 }
-Complex* multiplicationOfComplexNumbers(Complex number1, Complex number2) {
-	Complex* multiplication = new Complex{ number1.real * number2.real - number1.imaginary * number2.imaginary,
+Complex multiplicationOfComplexNumbers(const Complex& number1, const Complex& number2) {
+	Complex multiplication { number1.real * number2.real - number1.imaginary * number2.imaginary,
 	number1.real * number2.imaginary + number1.imaginary * number2.real };
 	return multiplication;
 
@@ -17,11 +17,10 @@ Complex* multiplicationOfComplexNumbers(Complex number1, Complex number2) {
 int main() {
 	Complex number1 = { 2,2 };
 	Complex number2 = { 4,3 };
-	 
-	Complex* sum = sumOfComplexNumbers(number1, number2);
-	Complex* multiplication = multiplicationOfComplexNumbers(number1, number2);
-	std::cout << "Sum of to comlex numbers: " << sum->real << " + " << sum->imaginary << "i\n";
-	std::cout << "Multiplication of to comlex numbers: " << multiplication->real << " + " << multiplication->imaginary << "i\n";
-	delete sum;
-	delete multiplication;
+
+	Complex sum = sumOfComplexNumbers(number1, number2);
+	Complex multiplication = multiplicationOfComplexNumbers(number1, number2);
+	std::cout << "Sum of two comlex numbers: " << sum.real << " + " << sum.imaginary << "i\n";
+	std::cout << "Multiplication of to comlex numbers: " << multiplication.real << " + " << multiplication.imaginary << "i\n";
+
 }
