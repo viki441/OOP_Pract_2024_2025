3e5fe7030ff0d326f749f6c7b36b4c9224f4dd11
Viki <91424361+viki441@users.noreply.github.com>
2025-05-05 15:52:18 +0300

    Update 01task.cpp

--- Pract 03/01task.cpp
+++ Pract 03/01task.cpp
@@ -1,49 +1,37 @@
 #include <iostream>
 #include <fstream>
-//for styling
 #include <iomanip>
 #include <string>
-//of - write; if - read
+
 int main() {
 	int a, b;
 	std::cout << "Pick 2 numbers: \n";
 	std::cin >> a >> b;
 	int sum = a + b;
 	int subtraction = a - b;
-	std::ofstream inputFile("result1.txt", std::ios::out);
-	if (!inputFile.is_open()) {
-		std::cerr << "Error opening file";
-		return 1;
+	std::ofstream writeFile("C:/Users/vikik/Desktop/example.txt");
+	if (!writeFile.is_open()) 
+	{
+		std::cerr << "Error opening file to write";
 	}
 
 	//input
-	inputFile << "Sum of numbers:";
-	inputFile << a + b << std::endl;
-	inputFile << "Subtraction of numbers:";
-	inputFile << a - b << std::endl;
+	writeFile << sum << std::endl;
+	writeFile << subtraction << std::endl;
 
-	inputFile.close();
+	writeFile.close();
 
-	std::ifstream outputFile("C:/Users/vikik/Desktop/example.txt");
-	std::cout << "Here is the sum and subtraction from the numbers: ";
-	char ch;
-	bool skip = false;
-	while (outputFile.get(ch)) {
-		if (ch == 'S') {
-			skip = true;
-		}
-		if (skip) {
-			if (ch == ':') {
-				skip = false; 
-			}
-			continue;
-		}
-		std::cout << ch;
-	}
+	std::ifstream readFile("C:/Users/vikik/Desktop/example.txt");
+	std::cout << "Here is the sum and subtraction from the numbers: \n";
+
+	int num1, num2;
+	readFile >> num1 >> num2;
+	std::cout << num1 << "\n" << num2;
+
 	int a_reconstructed = (sum + subtraction) / 2;
 	int b_reconstructed = sum - a_reconstructed;
-	std::cout << "The original numbers were: " << a_reconstructed << " and " << b_reconstructed;
+	std::cout << "\nThe original numbers were: " << a_reconstructed << " and " << b_reconstructed;
 
-	outputFile.close();
+	readFile.close();
 
 }
