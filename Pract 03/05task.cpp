4c674c09fce62483ca7d70a8d37333e7dac27392
Viki <91424361+viki441@users.noreply.github.com>
2025-05-05 19:15:59 +0300

    Update 05task.cpp

--- Pract 03/05task.cpp
+++ Pract 03/05task.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+namespace Constants
+{
+	const int SIZE = 25;
+}
 struct Pair {
 	int x;
 	int y;
@@ -8,14 +12,13 @@ struct Pair {
 
 struct Relation {
 	//array of pairs
-	static const int SIZE = 25;
-	Pair pairs[SIZE];
+	Pair pairs[Constants::SIZE];
 	int currSize;
 
 	//constructor for adding
 	Relation() : currSize(0) {
 
-		for (int i = 0; i < SIZE; ++i) {
+		for (int i = 0; i < Constants::SIZE; ++i) {
 			pairs[i].x = 0;
 			pairs[i].y = 0;
 		}
@@ -23,7 +26,7 @@ struct Relation {
 
 	//adds a pair to the relation
 	void addPair(int x, int y) {
-		if (currSize < SIZE) {
+		if (currSize < Constants::SIZE) {
 			pairs[currSize].x = x;
 			pairs[currSize].y = y;
 			currSize++;
@@ -33,8 +36,8 @@ struct Relation {
 		}
 	}
 	void putInFile(std::ofstream& file, const int number) const {
-		if (number < SIZE) {
-			
+		if (number < Constants::SIZE) {
+
 			file << "Chosen pair " << number << ": (" << pairs[number].x << ", " << pairs[number].y << ")\n";
 
 		}
@@ -45,14 +48,14 @@ struct Relation {
 	}
 	//put everything in file
 	void writeEverything(std::ofstream& file) {
-		for (int i = 0; i < SIZE; i++) {
+		for (int i = 0; i < Constants::SIZE; i++) {
 			file << "Pair " << i << ": (" << pairs[i].x << ", " << pairs[i].y << ")\n";
 		}
-	
+
 	}
 	void readFromFile(std::ifstream& file, const int number) const {
 		char ch;
-		if (number < SIZE) {
+		if (number < Constants::SIZE) {
 			while (file.get(ch)) {
 				if (ch == '\n') {
 					return;
@@ -66,24 +69,24 @@ struct Relation {
 	}
 };
 
-	int main() {
-		std::ofstream writeFile("C:\\Users\\vikik\\Desktop\\example.txt", std::ios::app);
-		int n;
-		std::cout << "Pick a number of relation: ";
-		std::cin >> n;
-		Relation r;
-		r.addPair(1, 4);
-		r.addPair(4, 4);
-		r.addPair(2, 7);
+int main() {
+	std::ofstream writeFile("C:\\Users\\vikik\\Desktop\\example.txt", std::ios::app);
+	int n;
+	std::cout << "Pick a number of relation: ";
+	std::cin >> n;
+	Relation r;
+	r.addPair(1, 4);
+	r.addPair(4, 4);
+	r.addPair(2, 7);
 
 
-		r.putInFile(writeFile, n);
-		r.writeEverything(writeFile);
-		writeFile.close();
+	r.putInFile(writeFile, n);
+	r.writeEverything(writeFile);
+	writeFile.close();
 
-		std::ifstream readFile("example.txt");
-		r.readFromFile(readFile, n);
-		readFile.close();
+	std::ifstream readFile("example.txt");
+	r.readFromFile(readFile, n);
+	readFile.close();
 
-	
+
 }
